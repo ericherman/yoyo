ea2ed5ccd6cd652f19dfec4822819e749dfd574e
Eric Herman <eric@freesa.org>
2020-09-20 09:41:22 +0000

    Progrefs in C

--- hangcheck.c
+++ hangcheck.c
@@ -20,12 +20,75 @@
 #define HANG_CHECK_INTERVAL 5
 #endif
 
+#ifndef MAX_HANGS
+#define MAX_HANGS 5
+#endif
+
+#ifndef MAX_RETRIES
+#define MAX_RETRIES 5
+#endif
+
 #ifndef VERBOSE
 #define VERBOSE 0
 #endif
 
+#define Errnof(format, ...) \
+	errnof(__FILE__, __LINE__, format __VA_OPT__(,) __VA_ARGS__)
+
+/* prototypes */
+static void do_parent(unsigned int *retries_remaining, pid_t childpid);
+
+int process_looks_hung(pid_t pid);
+
 int exit_reason(pid_t child_pid, int wait_status, int *exit_code, char *buf,
 		size_t buf_len);
+
+void errnof(const char *file, int line, const char *format, ...);
+
+int appendf(char *buf, size_t bufsize, const char *format, ...);
+
+/* the POSIX definition of "signal()" does not allow for a context parameter:
+	#include <signal.h>
+
+       typedef void (*sighandler_t)(int);
+
+       sighandler_t signal(int signum, sighandler_t handler);
+
+  thus, we will need a global variable to pass data between functions. */
+/* globals */
+
+static int child_trap_exit_code_singleton;
+
+/* functions */
+int main(int argc, char **argv)
+{
+	if (argc < 2) {
+		fprintf(stderr, "no child command?\n");
+		exit(EXIT_FAILURE);
+	}
+
+	unsigned int retries_remaining = MAX_RETRIES;
+	while (retries_remaining > 0) {
+		child_trap_exit_code_singleton = INT_MIN;
+		pid_t childpid = fork();
+
+		if (childpid < 0) {
+			Errnof("fork() failed?");
+			exit(EXIT_FAILURE);
+		} else if (childpid > 0) {
+			do_parent(&retries_remaining, childpid);
+		} else {
+			// shift-off this program and exec the rest:
+			char **rest = argv + 1;
+			int rv = execv(rest[0], rest);
+			exit(rv);
+		}
+	}
+
+	fflush(stdout);
+	fprintf(stderr, "Retries limit reached.\n");
+	return EXIT_FAILURE;
+}
 
 int process_looks_hung(pid_t pid)
 {
@@ -34,70 +97,60 @@ int process_looks_hung(pid_t pid)
 
 void child_trap(int sig)
 {
-	printf("Child died (or stopped but let's ignore that)\n");
+	if (VERBOSE) {
+		printf("child_trap(%d)\n", sig);
+	}
 	int wstatus = 0;
 	int options = 0;
 	pid_t any_child = -1;
 	pid_t pid = waitpid(any_child, &wstatus, options);
-	int exit_code = INT_MIN;
 
-	char buf[255];
-	int exited = exit_reason(pid, wstatus, &exit_code, buf, 255);
+	size_t len = 255;
+	char buf[len];
+	int *exit_code_ptr = &child_trap_exit_code_singleton;
+	int exited = exit_reason(pid, wstatus, exit_code_ptr, buf, len);
+
 	if (VERBOSE) {
 		printf("%s\n", buf);
 	}
 
 	if (exited) {
-		printf("Child process %d status: %d\n", pid, exit_code);
-		exit(EXIT_SUCCESS);
+		if (child_trap_exit_code_singleton == 0) {
+			printf("Child completed successfully\n");
+			exit(EXIT_SUCCESS);
+		} else {
+			printf("Child exited with status %d\n",
+			       child_trap_exit_code_singleton);
+		}
 	}
 }
 
-int main(int argc, char **argv)
+static void do_parent(unsigned int *retries_remaining, pid_t childpid)
 {
-	pid_t childpid = fork();
-
-	if (childpid > 0) {	// WE IS PARENT
-		signal(SIGCHLD, &child_trap);
-		int hang_count = 0;
-		for (;;) {
-			unsigned int seconds = HANG_CHECK_INTERVAL;
-			unsigned int remain = sleep(seconds);
-			if (process_looks_hung(childpid)) {
-				++hang_count;
-				int sig = hang_count > 3 ? SIGTERM : SIGKILL;
-				int err = kill(childpid, sig);
-				if (err) {
-					int save_errno = errno;
-					fflush(stdout);
-					fprintf(stderr,
-						"kill(%d) failed?"
-						" errno %d: %s\n", sig,
-						save_errno,
-						strerror(save_errno));
-				}
-			} else {
-				hang_count = 0;
-				printf("Child still appears to be"
-				       " doing something worthwhile\n");
-			}
+	signal(SIGCHLD, &child_trap);
+	int hang_count = 0;
+	for (;;) {
+		if (child_trap_exit_code_singleton != INT_MIN) {
+			--(*retries_remaining);
+			break;
 		}
-	} else if (childpid == 0) {	// WE IS CHILD
-		const char *path = argv[1];
-		char **argv2 = argv + 1;
-		int rv = execv(path, argv2);
-		exit(rv);
-	} else {
-		int save_errno = errno;
-		fflush(stdout);
-		fprintf(stderr, "fork failed? errno %d: %s\n", save_errno,
-			strerror(save_errno));
-		exit(EXIT_FAILURE);
+		unsigned int seconds = HANG_CHECK_INTERVAL;
+		unsigned int remain = sleep(seconds);
+		(void)remain;	// ignore
+		if (process_looks_hung(childpid)) {
+			++hang_count;
+			int sig = hang_count > MAX_HANGS ? SIGTERM : SIGKILL;
+			int err = kill(childpid, sig);
+			if (err) {
+				Errnof("kill(childpid, %d) returned %d?", sig,
+				       err);
+			}
+		} else {
+			hang_count = 0;
+			printf("Child still appears to be"
+			       " doing something worthwhile\n");
+		}
 	}
-
-	fflush(stdout);
-	fprintf(stderr, "how did we get here?\n");
-	return 1;
 }
 
 int appendf(char *buf, size_t bufsize, const char *format, ...)
@@ -114,6 +167,22 @@ int appendf(char *buf, size_t bufsize, const char *format, ...)
 	buf[bufsize - 1] = '\0';
 
 	return printed;
+}
+
+void errnof(const char *file, int line, const char *format, ...)
+{
+	int _errnof_save = errno;
+
+	fflush(stdout);
+	fprintf(stderr, "%s:%d ", file, line);
+
+	va_list args;
+	va_start(args, format);
+	vfprintf(stderr, format, args);
+	va_end(args);
+
+	fprintf(stderr, " errno %d: %s\n", _errnof_save,
+		strerror(_errnof_save));
 }
 
 int exit_reason(pid_t child_pid, int wait_status, int *exit_code, char *buf,

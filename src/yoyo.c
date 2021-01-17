3d0dad9832304804c880cea7a3488083755a0177
Eric Herman <eric@freesa.org>
2021-01-17 09:37:00 +0100

    fix spacing/wording in error log

--- src/yoyo.c
+++ src/yoyo.c
@@ -182,9 +182,9 @@ int yoyo(int argc, char **argv)
 		} else {
 			char er_buf[250];
 			exit_reason_to_str(&global_exit_reason, er_buf, 250);
-			Ylog(0, "exit reason:%s\n", er_buf);
+			Ylog(0, "exit reason: %s\n", er_buf);
 
-			snprintf(buf, buflen, "Child '%s' ended oddly\n",
+			snprintf(buf, buflen, "Child '%s' killed\n",
 				 child_command_line[0]);
 			Ylog(0, "%s", buf);
 			strcat(summary, buf);
